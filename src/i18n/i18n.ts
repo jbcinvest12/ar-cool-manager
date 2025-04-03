712298e93c9bc769eac496ed089d07361a09667f
jbcinvest12 <jbcinvest12@gmail.com>
2025-04-02 23:28:02 -0300

    *

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,25 +1,36 @@
-
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import ptBR from './locales/pt-BR';
-import enUS from './locales/en-US';
-import esES from './locales/es-ES';
+
+import translationPtBr from './locales/pt-BR/translation.json';
+import translationEnUs from './locales/en-US/translation.json';
+import translationEsEs from './locales/es-ES/translation.json';
+
+// the translations
+const resources = {
+  'pt-BR': {
+    translation: translationPtBr
+  },
+  'en-US': {
+    translation: translationEnUs
+  },
+  'es-ES': {
+    translation: translationEsEs
+  }
+};
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      'pt-BR': ptBR,
-      'en-US': enUS,
-      'es-ES': esES,
-    },
+    resources,
     fallbackLng: 'pt-BR',
-    debug: process.env.NODE_ENV === 'development',
+    debug: false,
+
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // not needed for react as it escapes by default
     },
+
     detection: {
       order: ['localStorage', 'navigator'],
       caches: ['localStorage'],
